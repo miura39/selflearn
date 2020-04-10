41efa23864899e98f9a87a319f86bbe8ae991b53
miura39 <miurast.miura.y@gmail.com>
2020-04-10 12:40:34 +0900

    test

--- src/to/msn/wings/selflearn/chap01/Hello.java
+++ src/to/msn/wings/selflearn/chap01/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		LocalDateTime time = LocalDateTime.now();
 		System.out.println(time);
-
+		System.out.println("test");
+		
 	}
-
 }
