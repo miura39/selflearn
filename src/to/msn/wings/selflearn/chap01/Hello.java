8a5b369ca90d51cb7cb0b8f96a61ddb43ca8101a
miura39 <miurast.miura.y@gmail.com>
2020-04-15 12:32:09 +0900

    study

--- src/to/msn/wings/selflearn/chap01/Hello.java
+++ src/to/msn/wings/selflearn/chap01/Hello.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 
 public class Hello {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         LocalDateTime time = LocalDateTime.now();
           System.out.println(time);
@@ -90,38 +93,70 @@ public class Hello {
             		 System.out.println(array6[i]+"点ー＞不合格");
             	 }
              }
-             
+
              String sympol = "*";
              for(int i = 0; i < 5; i++) {
-            	 for(int j = 0; j< 1+i; j++) {
+            	 for(int j = 0; j< i+1; j++) {
             		 System.out.print(sympol);
             	 }
             	 System.out.println();
              }
-             
+
              String sympol2 = "*";
              for(int i = 0; i < 5; i++) {
-            	 for(int j = 4; j>i-1; j--) {
+            	 for(int j = 5; j > i; j--) {
             		 System.out.print(sympol2);
             	 }
             	 System.out.println();
              }
 
-
-
-
-
+             String[] str1 = {"テスト","テスト２","テスト３"};
+             for(int i = 0; i<3;i++) {
+            	 System.out.println(str1[i]);
+             }
 
 //          int loopMax = 5;
 //          for(int loopCount = 0; loopCOunt<loopMax;loopCount++) {
 //        	  System.out.println("loopCountは" + loopCount)
 //          }
+             byte val[] = new byte[4];
+             val[0] = 0x48;
+             val[1] = 0x4f;
+             val[2] = 0x47;
+             val[3] = 0x45;
+
+             byte val2[] = new byte[1];
+             val2[0] = 0x61;
+
+             //画面表示
+             System.out.println(new String(val));
+             System.out.println(new String(val2));
+
+
+             int[][] mtrx = {{10,20,30,40},{100,200},{1,2,3,4,5}};
+
+             System.out.println("全要素表示");
+             for(int i = 0; i<mtrx.length; i++) {
+            	 for(int j = 0; j<mtrx[i].length; j++) {
+            		 System.out.print(mtrx[i][j] + "  ");
+              }
+            	 System.out.println();
+             }
+             System.out.println();
+
+             for (int i =0; i<mtrx.length; i++) {
+//            	 System.out.println(mtrx[1][2]);
+             }
 
     }
 
 	private static boolean strequals(String string) {
 		// TODO 自動生成されたメソッド・スタブ
 		return false;
+	}
+	public static void dispData(String a,String c, int b) {
+		System.out.println("名前は"+a+c);
+		System.out.println("年齢は"+b+"歳");
 	}
 }
 
