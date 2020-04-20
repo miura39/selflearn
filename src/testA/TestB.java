ada529566f780fe7ea0e02a8a9864bc7f5ba75af
miura39 <miurast.miura.y@gmail.com>
2020-04-20 23:46:18 +0900

    class_study

---
+++ src/testA/TestB.java
@@ -0,0 +1,10 @@
+package testA;
+
+public class TestB {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- src/testA/TextA.java
+++
@@ -1,5 +0,0 @@
-package testA;
-
-public class TextA {
-
-}
