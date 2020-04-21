3c167f7d3eeaeeb4088eb345467dc50d5b681dc6
miura39 <miurast.miura.y@gmail.com>
2020-04-21 22:20:20 +0900

    java　コンストラクタ引数

---
+++ src/testA_4/TestB.java
@@ -0,0 +1,13 @@
+package testA_4;
+
+public class TestB {
+
+	public static void main(String[] args) {
+		//コンストラクタ引数
+		TestA str = new TestA("E01","Betty",85,90,80,100);
+
+
+		str.dispStudent();
+
+	}
+}
