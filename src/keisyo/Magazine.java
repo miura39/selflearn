fd9c61ffef26a5a1d6c23ac3ac6849818b4f48c8
miura39 <miurast.miura.y@gmail.com>
2020-04-24 23:20:33 +0900

    継承

---
+++ src/keisyo/Magazine.java
@@ -0,0 +1,5 @@
+package keisyo;
+
+public class Magazine {
+
+}
---
+++ src/keisyo/UseMagazine.java
@@ -0,0 +1,10 @@
+package keisyo;
+
+public class UseMagazine {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
