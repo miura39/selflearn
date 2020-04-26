4cab66da4b7edb5946cc89189f6529543db92e31
miura39 <miurast.miura.y@gmail.com>
2020-04-26 23:01:40 +0900

    test

--- src/keisyo/UseMagazine.java
+++ src/keisyo/UseMagazine.java
@@ -3,7 +3,13 @@ package keisyo;
 public class UseMagazine {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
+		//Magagine型クラスのインスタンス化
+		Mabagine text1 = new Magazine("superfiled","superfrequency","bookisbn",
+									  "bookbookname","bookauthor",100);
+
+		System.out.println(text1);
+
 
 	}
 
