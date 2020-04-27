b7e323733eb8bdb2dccea92d0840051127dca54e
miura39 <miurast.miura.y@gmail.com>
2020-04-27 23:55:00 +0900

    HashMapの利用

--- src/test7_2/UseRecord.java
+++ src/test7_2/UseRecord.java
@@ -1,24 +1,25 @@
 package test7_2;
-import java.util.ArrayList;
-import java.util.List;
+//HashMapとMapのインポート宣言
+import java.util.HashMap;
+import java.util.Map;
 
 public class UseRecord {
 
 	public static void main(String[] args) {
 
-		//ArrayList宣言　<>の中身はどんなインスタンスも格納可能。new後の<>はダイヤモンド演算子。左辺の<>に基づいて右辺の<>を推測する。
-		List<Record>textsList=new ArrayList<>();
+		//HashMapの宣言<>はキーの型、値の型　マップ変数＝・・・・。new後の<>はダイヤモンド演算子。左辺の<>に基づいて右辺の<>を推測する。
+		Map<String,Record>textsList=new HashMap<>();
 
-		//ArrayList生成
-		textsList.add(new Record("1A","Betty",85,90,80,100));
-		textsList.add(new Record("2A","Betty",85,90,80,100));
-		textsList.add(new Record("3A","Betty",85,90,80,100));
-		textsList.add(new Record("4A","Betty",85,90,80,100));
-		textsList.add(new Record("5A","Betty",85,90,80,100));
+		//Recordクラス型をインスタンス化してHashMapへ格納
+		textsList.put("1A",new Record("1A","Betty",85,90,80,100));
+		textsList.put("2A",new Record("2A","Betty",85,90,80,100));
+		textsList.put("3A",new Record("3A","Betty",85,90,80,100));
+		textsList.put("4A",new Record("4A","Betty",85,90,80,100));
+		textsList.put("5A",new Record("5A","Betty",85,90,80,100));
 
-		//拡張for文
-		for(Record text:textsList) {
-			System.out.println(text);
+		//HashMapの拡張for文（出力順番はバラバラになってOK）
+		for(String text:textsList.keySet()) {
+			System.out.println(textsList.get(text));
 		}
 	}
 }
