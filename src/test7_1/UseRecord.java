69812b505f3380e562f859a79bf92ea18b869b8e
miura39 <miurast.miura.y@gmail.com>
2020-04-27 23:16:05 +0900

    arrayListのコメント追加

--- src/test7_1/UseRecord.java
+++ src/test7_1/UseRecord.java
@@ -6,8 +6,8 @@ public class UseRecord {
 
 	public static void main(String[] args) {
 
-		//ArrayList宣言
-		List<Record>textsList=new ArrayList<Record>();
+		//ArrayList宣言　<>の中身はどんなインスタンスも格納可能。new後の<>はダイヤモンド演算子。左辺の<>に基づいて右辺の<>を推測する。
+		List<Record>textsList=new ArrayList<>();
 
 		//ArrayList生成
 		textsList.add(new Record("1A","Betty",85,90,80,100));
@@ -20,7 +20,5 @@ public class UseRecord {
 		for(Record text:textsList) {
 			System.out.println(text);
 		}
-
-
 	}
 }
