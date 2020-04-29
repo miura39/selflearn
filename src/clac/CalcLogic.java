eede8c6fb42ee1fda8e926611b14815064d45f2e
miura39 <miurast.miura.y@gmail.com>
2020-04-30 01:09:27 +0900

    計算クラスを分ける

---
+++ src/clac/CalcLogic.java
@@ -0,0 +1,13 @@
+package clac;
+
+public class CalcLogic {
+	//足すクラスの作成
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	//引くクラスの作成
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+
+}
--- src/clac/UseCalc.java
+++ src/clac/UseCalc.java
@@ -5,15 +5,11 @@ public class UseCalc {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 3;
-		int total = tasu(a,b);
-		int delta = hiku(a,b);
+		//足すクラスのメソッドの利用
+		int total = CalcLogic.tasu(a,b);
+		//引くクラスのメソッドの利用
+		int delta = CalcLogic.hiku(a,b);
+		//結果：足すと13、引くと7
 		System.out.println("足すと" + total + "、引くと" + delta);
 	}
-	public static int tasu(int a, int b) {
-		return(a+b);
-	}
-	public static int hiku(int a, int b) {
-		return(a-b);
-	}
-
 }
