90604e4ce8f00e7d605cdc14c9b50d894b69d614
miura39 <miurast.miura.y@gmail.com>
2020-04-30 01:27:15 +0900

    別のパッケージクラスのメソッドを利用する

---
+++ src/calcsecond/CalcLogicSecond.java
@@ -0,0 +1,8 @@
+package calcsecond;
+
+public class CalcLogicSecond {
+	//別パッケージ(calc)で利用される掛け算のロジック
+	public static int kakeru(int a,int b) {
+		return(a * b);
+	}
+}
--- src/clac/UseCalc.java
+++ src/clac/UseCalc.java
@@ -1,4 +1,6 @@
 package clac;
+//パッケージ名.クラス名で別パッケージのロジックを利用出来る
+import calcsecond.CalcLogicSecond;
 
 public class UseCalc {
 
@@ -9,7 +11,11 @@ public class UseCalc {
 		int total = CalcLogic.tasu(a,b);
 		//引くクラスのメソッドの利用
 		int delta = CalcLogic.hiku(a,b);
+		//別パッケージの掛け算クラスの利用
+		int mul = CalcLogicSecond.kakeru(a,b);
 		//結果：足すと13、引くと7
 		System.out.println("足すと" + total + "、引くと" + delta);
+		//結果：掛け算すると30
+		System.out.println("掛け算すると" + mul);
 	}
 }
