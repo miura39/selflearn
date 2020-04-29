66818d47faf8c5253056116132311c77e19d8947
miura39 <miurast.miura.y@gmail.com>
2020-04-29 23:17:23 +0900

    戻り値をそのまま利用する場合

--- src/returnVerification/Usemain.java
+++ src/returnVerification/Usemain.java
@@ -5,7 +5,13 @@ public class Usemain {
 	public static void main(String[] args) {
 
 		int ans = add(100,10);
+		//addメソッドで計算された戻り値のデータ算出
 		System.out.println(" x + y "+ ans);
+
+		//戻り値をそのまま利用する場合
+		System.out.println(" x + y " + add(100,100));//結果は２００を返す
+
+
 
 	}
 		public static int add(int x,int y) {
