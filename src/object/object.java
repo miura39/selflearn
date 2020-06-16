d4d3fc8b738667f9670280fe768b44d4deed4b03
miura39 <miurast.miura.y@gmail.com>
2020-06-16 23:30:11 +0900

    objecttest

---
+++ src/object/object.java
@@ -0,0 +1,12 @@
+package object;
+
+public class object {
+
+	public static void main(String[] args) {
+		int yusha_hp =100;
+		int matango_hp=10;
+		String yusha_name = "yusya";
+
+		System.out.println(yusha_name + yusha_hp + matango_hp);
+	}
+}
