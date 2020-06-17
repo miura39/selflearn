d3321775fd67bffd2874af12164532a4487c0cb1
miura39 <miurast.miura.y@gmail.com>
2020-06-17 23:57:59 +0900

    test

--- src/object/object.java
+++ src/object/object.java
@@ -2,11 +2,12 @@ package object;
 
 public class object {
 
-	public static void main(String[] args) {
 		int yusha_hp =100;
 		int matango_hp=10;
 		String yusha_name = "yusya";
 
-		System.out.println(yusha_name + yusha_hp + matango_hp);
+	public void sleep() {
+		this.yusha_hp = 100;
+		System.out.println(this.yusha_hp);
 	}
 }
