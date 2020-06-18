47a96bcd36d6cfc96a6fc1a619dd75214e9f3072
miura39 <miurast.miura.y@gmail.com>
2020-06-18 23:32:25 +0900

    アスタリスク一つずつ増やす

---
+++ src/algorithm/Ensyu8_1.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class Ensyu8_1 {
+
+	public static void main(String[] args) {
+		//初期値設定
+		int max = 5;
+
+			for(int i=0;i < max;i++) {
+				for(int j=0;j < i+1; j++) {
+					System.out.print("*");
+				}
+			System.out.println();
+		}
+	}
+}
